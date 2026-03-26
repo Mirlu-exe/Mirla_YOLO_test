27f92fae3fc96a9be907ff5b95d3bfaa7e232605
Mirlu-exe <mirlu.exe@outlook.com>
2026-03-26 20:24:37 +0900

    latest working version 2026

--- predict_alt.py
+++ predict_alt.py
@@ -27,7 +27,7 @@ INPUT_VIDEO = VIDEOS_DIR / '250825_10session_site1_1.MP4' #name of the new video
 OUTPUT_VIDEO = INPUT_VIDEO.with_name(INPUT_VIDEO.stem + '_tracked.mp4')
 
 ## Custom model (fine-tuned elephants)
-MODEL_PATH = Path('./runs/detect/train29/weights/last.pt')
+MODEL_PATH = Path('./runs/detect/train36/weights/last.pt')
 
 CHUNK_SECONDS = 180            # process in N-second chunks
 TARGET_HEIGHT = 720            # resize height (keeps aspect ratio)
